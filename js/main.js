dcc7d5d5ad4e7c8aee2ad4d4b3145bde5da62ed8
Adam Buffett <adam_buffett@outlook.com>
2023-05-10 11:15:29 -0400

    Add dark mode to the landing page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('JS is connected to main')
+/*----- cached elements  -----*/
+const body = document.querySelector('body')
+const header = document.querySelector('header')
+const promt = document.querySelector('#gamePromt')
+const rules = document.querySelector('article')
+const ruleList = document.querySelector('ul')
+const startGame = document.querySelector('#startGame')
+
+const toggleDarkMode = (e) => {
+  e.target.classList.toggle('dark')
+  e.target.innerText = e.target.innerText.includes('Dark')
+    ? 'Light Mode'
+    : 'Dark Mode'
+  body.classList.toggle('dark')
+  header.classList.toggle('dark')
+  promt.classList.toggle('dark')
+  rules.classList.toggle('dark')
+  ruleList.classList.toggle('dark')
+  startGame.classList.toggle('dark')
+}
+
+document.querySelector('#toggleDark').addEventListener('click', toggleDarkMode)
